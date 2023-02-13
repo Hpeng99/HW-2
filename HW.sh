35ad8a542e7996d5ce4c1cb3ae9707744f093bdc
Hpeng99 <122468233+Hpeng99@users.noreply.github.com>
2023-02-13 11:47:30 -0500

    Add files via upload

--- HW.sh
+++ HW.sh
@@ -7,7 +7,6 @@ Echo $Greeting $Name
 mkdir learning_bash
 cd learning_bash
 mkdir bioinfomatics
-cd '/cygdrive/c/Users/Hao Peng/Desktop/Bioinfo' 
-cp transcriptome.fasta '/cygdrive/c/Users/Hao Peng/Desktop/Bioinfo/learning_bash/bioinfomatics'
+cp "/cygdrive/c/Users/Hao Peng/Desktop/Bioinfo/transcriptome.fasta" '/cygdrive/c/Users/Hao Peng/Desktop/Bioinfo/learning_bash/bioinfomatics'
 cd '/cygdrive/c/Users/Hao Peng/Desktop/Bioinfo/learning_bash/bioinfomatics'
 grep -c ">" transcriptome.fasta >> fasta_count.txt '/cygdrive/c/Users/Hao Peng/Desktop/Bioinfo/learning_bash/bioinfomatics'
